fdbab8d552e628c1ca401213272c6069e5e88fe4
Yizi-Yeh <g384357@gmail.com>
2021-05-11 08:30:36 +0800

    以 async fun ( ) { await apiGetPhotoList( ) }方式改寫 res.then( ) 的方式

---
+++ async_await.js
@@ -0,0 +1,18 @@
+ handInit({ commit }) {
+          const res = apiGetPhotoList();
+          return res.then((response) => {
+            commit("init", response.data);
+            return response.data;
+          });
+        },
+    // --------------------------------------------------->
+    // 以 async await 方式改寫 return 的方式
+     async handInit({ commit }) {
+              try {
+                const res = await apiGetPhotoList();
+                commit("setPhoto", res.data);
+                return res.data;
+              } catch (error) {
+                console.error(error);
+              }
+            },
