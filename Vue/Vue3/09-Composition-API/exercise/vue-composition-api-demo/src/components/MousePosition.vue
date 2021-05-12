a754792221ecede5fdd6e8e494cfc41c782b23e6
Yizi-Yeh <g384357@gmail.com>
2021-05-12 09:15:46 +0800

    使用 toRefs 方式，包裝 reactive 物件，綁定解構後的資料

--- Vue/Vue3/09-Composition-API/exercise/vue-composition-api-demo/src/components/MousePosition.vue
+++ Vue/Vue3/09-Composition-API/exercise/vue-composition-api-demo/src/components/MousePosition.vue
@@ -3,10 +3,15 @@ import { useMousePosition } from '../composition-api'
 export default {
   setup() {
 
-    // 把 x , y 從 MousePosition()解構出來
-    const { x,y } = useMousePosition()
-    // 丟到模板上
-    return { x,y };
+    // 當透過解構取出 reactive 中的值時，會變成單純的變數
+    // 不具備資料綁定的功能
+    // 等同於任意變數 
+    // 改為 pos
+
+    // 透過 toRefs 方法，讓解構後的值具備綁定資料的效果
+    const { x,y,name } = useMousePosition()
+
+    return { x,y,name } 
   },
 };
 </script>
@@ -14,7 +19,7 @@ export default {
 <template>
   <div class="box">
     <h1>Mouse Position</h1>
-    <h2>x:{{ x }} ,y:{{ y }}</h2>
+    <h2>x:{{ x }} ,y:{{ y }} {{ name }}</h2>
   </div>
 </template>
 
--- Vue/Vue3/09-Composition-API/exercise/vue-composition-api-demo/src/composition-api/useMousePosition.js
+++ Vue/Vue3/09-Composition-API/exercise/vue-composition-api-demo/src/composition-api/useMousePosition.js
@@ -1,26 +1,27 @@
-import { ref,onMounted,onUnmounted } from 'vue'
+import { ref,reactive, toRefs,onMounted,onUnmounted } from 'vue'
 export function MousePosition() {
 
-    const x = ref(0)
-    const y = ref(0)
+    // 改以 reactive 做此功能
+    const pos = reactive({
+        x:0,
+        y:0
+    })
 
-    // 抓取滑鼠位置
+    const name = ref('yizi')
+
     const posUpdate = (e) =>{
-        x.value = e.pageX
-        y.value = e.pageY
+        pos.x = e.pageX
+        pos.y = e.pageY
     }
     
-    // 做什麼？ -> 監聽 mousemove
-    // 何時監聽？ -> DOM 元素 render 後監聽
    onMounted(()=>{
     window.addEventListener('mousemove',posUpdate)
    }) 
 
-    // 何時不做？
    onUnmounted(()=>{
     window.removeEventListener('mousemove',posUpdate)
    }) 
 
-   // mousemove 監聽值，最後將它 return 出去
-   return  { x,y }
+    // return reactive pos
+   return  {...toRefs(pos),name}
 }
