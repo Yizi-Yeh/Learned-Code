6b8fecde6bc0a5a24fcaca9902a11d7e75bcbc77
Yizi-Yeh <g384357@gmail.com>
2021-05-18 09:05:44 +0800

    props各式型別的預設傳遞方法，若要傳遞 arr 或是 obj 需要透過 function return

---
+++ Vue/Vue3/04-VUE-CLI-Router-Components/props-demo/src/App.vue
@@ -0,0 +1,85 @@
+<script>
+import propsTest2 from "@/components/propsTest2.vue";
+
+import { reactive, ref } from "vue";
+// 放到畫面上
+export default {
+  components: {
+    propsTest2,
+  },
+  // 父層要傳資料給子組件
+  setup() {
+  const str = ref('hello world')
+
+  const bool = ref(true)
+
+  const num = ref(0)
+
+  const arr = reactive([])
+
+  const obj = reactive({})
+
+  const handclickfn = () => {}
+   
+    return {
+      str,
+      bool,
+      num,
+      arr,
+      handclickfn,
+
+    };
+  },
+};
+</script>
+
+<template>
+<propsTest2 
+:str="str"
+:bool="bool"
+:num="num"
+:arr="arr"
+:handclickfn="handclickfn"
+
+/>
+
+</template>
+
+<style>
+/* .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+} */
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: slategray;
+}
+#app {
+  width: 400px;
+  overflow: hidden;
+  border: 1px solid #42b983;
+  border-radius: 10px;
+}
+
+/* #box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+} */
+</style>
---
+++ Vue/Vue3/04-VUE-CLI-Router-Components/props-demo/src/components/propsTest2.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1> {{props.msg}}</h1>
+</template>
+
+<script>
+export default {
+    // 因傳遞資料時需要確定型別，故改以物件傳遞
+    // 傳進來的參數是字串
+    // 若傳錯型別會有 warning(但扔然會呈現)
+    props:{
+       str:{
+       type:String,
+       default:"default data"},
+
+       bool:{
+        type:Boolean,
+        default:true
+       },
+
+       num:{
+        type:Number,
+        default:0
+       },
+       arr:{
+           type:Array,
+           default: () => []
+       },
+
+       obj:{
+           type:Object,
+           default: () => ({})
+       },
+
+       handclickfn:{
+           type:Function,
+           default:() => {}
+       }
+       
+    },
+    
+    setup(props){
+        return{
+         props
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: antiquewhite;
+    font-size: 50px;
+    text-align: center;
+}
+</style>
