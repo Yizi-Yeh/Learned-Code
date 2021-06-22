483f60646f7256283c30d9d1bb8493d595bb246b
Yizi-Yeh <g384357@gmail.com>
2021-06-22 19:33:45 +0800

    update: 陣列方法/unshift() intro

--- 陣列方法/unshift.js
+++ 陣列方法/unshift.js
@@ -2,9 +2,10 @@
   const sortArray = (n) =>{
       let arr = []
     n.forEach(item=>{
+      // 使用 arr.unshift(item) ，將偶數排在陣列前位
        item%2 === 0 ? arr.unshift(item) : arr.push(item)
     })
     return arr
   }
 
-  sortArray([1,2.])
+  sortArray([1,2,3,4])
