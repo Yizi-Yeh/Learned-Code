963183236a610ab3903e138a233971233835c542
Yiz Yeh <g384357@gmail.com>
2022-10-11 20:44:36 +0800

    update find object with same id in two arrays

--- 陣列方法/findObjectWithSameIdInTwoArray.js
+++ 陣列方法/findObjectWithSameIdInTwoArray.js
@@ -1,2 +1,5 @@
-let result = arr1.filter((obj1) => arr2.some((obj2) => obj1.id === obj2.id));
+let result1 = arr1.filter((obj1) => arr2.some((obj2) => obj1.id === obj2.id));
 
+let result2 = array1.filter(element => array2.includes(element));
+
+let result3 = array1.filter(e => array2.indexOf(e) !== -1);
